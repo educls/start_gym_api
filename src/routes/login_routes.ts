0fa5b4dd6fdf5928ad385b7399bf925a1adb6be8
Educls <3duardocesar@gmail.com>
2024-05-28 07:55:44 -0300

    swagger login

--- src/routes/login_routes.ts
+++ src/routes/login_routes.ts
@@ -2,7 +2,29 @@ import express from 'express';
 const Router = express.Router();
 const login_controller = require('../controllers/login_controller')
 
+/**
+ * @swagger
+ * /login/:
+ *   post:
+ *     summary: Retorna o token de auth
+ *     responses:
+ *       200:
+ *         description: LOGIN
+ *         content:
+ *           application/json:
+ *             schema:
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   email:
+ *                     type: string
+ *                     example: email@email.com
+ *                   password:
+ *                     type: string
+ *                     example: senha123
+ */
 Router.post('/', login_controller.post);
 
 
 module.exports = Router;
+
